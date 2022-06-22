fec2b07b263285920a3c4234e46a8099249cbc87
Bruno-Valerio <alvesvaleriobruno@gmail.com>
2022-06-22 19:39:39 -0300

    atualizando projeto

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -7,6 +7,8 @@ namespace Model
 {
     public class Usuario
     {
-        
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime DataNascimento { get; set; }
     }
 }
---
+++ Usuario/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuario.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
