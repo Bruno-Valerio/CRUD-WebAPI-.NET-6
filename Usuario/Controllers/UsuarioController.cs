4d5400c36df5b6fae24f5204d99e8694cd97a6ac
Bruno-Valerio <alvesvaleriobruno@gmail.com>
2022-06-23 21:34:28 -0300

    atualizando projeto

--- Usuario/Controllers/UsuarioController.cs
+++ Usuario/Controllers/UsuarioController.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using usuario.Model;
 
-namespace Usuario.Controllers
+namespace usuario.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        private static List<Usuario> Usuarios()
         {
-            return "OK";
+            return new List<Usuario>{
+                new Usuario{ nome = "Bruno", id = 1, DataNascimento = new DateTime(1995, 2, 7)}
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Usuarios());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Usuario usuario)
+        {
+            var usuarios = Usuarios();
+            usuarios.Add(usuario);
+            return Ok(Usuarios());
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok(Usuarios());
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok(Usuarios());
         }
     }
 }
